46a57ae031ad7ae80829c6617f7a178d15387761
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-09-12 21:40:13 -0400

    adding components

---
+++ shopping-summary/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container'
+
+import Subtotal from './components/Subtotal/subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings';
+import TaxesFees from './components/TaxesFees/TaxesFees';
+import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
+import ItemDetails from './components/ItemDetails/ItemDetails';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100,
+      PickupSavings: -3.85,
+      taxes: 0,
+      estimatedTotal: 0
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <Container className="purchase-card">
+          <Subtotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
+          <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
+          <ItemDetails />
+        </Container>
+      </div>
+    );
+  }
+}
+export default App;
